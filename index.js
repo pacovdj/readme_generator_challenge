7b1e0c988027e667a89696013b2706950540455f
Mona <you@example.com>
2022-01-09 22:19:02 -0500

    created template

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ inquierer.prompt(
         {
          type: 'input',
          message: 'What is your name?',
-         name: 'first name',
+         name: 'fn',
          validate: (value)=> {if (value){return true} else {return 'you must input a value to continue'}},   
         },
         {
@@ -34,9 +34,35 @@ inquierer.prompt(
         {
         type: 'input',
         message: 'How do you use your app?',
-        name: 'using your app',
+        name: 'usage',
         validate: (value)=> {if (value){return true} else {return 'you must input a value to continue'}},   
         },
+        {
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: 'git',
+        validate: (value)=> {if (value){return true} else {return 'you must input a value to continue'}},    
+        }
+    ]
+    ).then(({
+        fn,
+        title,
+        installation,
+        instructions,
+        usage,
+        git,
+    })=>{
+    const template = `# ${title}
+    * [fn](#fn)
+    * [title](#title)
+    * [installation](#installation)
+    * [instructions](#instructions)
+    * [usage](#usage)
+    * [git](#git) 
+    `
+
+    }
+    )
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
